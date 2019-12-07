c921eef3d77a3b7c4055ec515a40ec23ce6e85c0
Matt Gale <matt@littleball.co.uk>
2019-12-07 22:14:16 +0000

    Day 5 - part 1
    Woop - took me a while

--- day05/code.js
+++ day05/code.js
@@ -1,26 +1,22 @@
-const input = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 2, 171, 209, 224, 1001, 224, -1040, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 223, 224, 223, 102, 65, 102, 224, 101, -3575, 224, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 223, 224, 223, 1102, 9, 82, 224, 1001, 224, -738, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1101, 52, 13, 224, 1001, 224, -65, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 6, 224, 1, 223, 224, 223, 1102, 82, 55, 225, 1001, 213, 67, 224, 1001, 224, -126, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 7, 224, 1, 223, 224, 223, 1, 217, 202, 224, 1001, 224, -68, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 1, 224, 1, 224, 223, 223, 1002, 176, 17, 224, 101, -595, 224, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 224, 223, 223, 1102, 20, 92, 225, 1102, 80, 35, 225, 101, 21, 205, 224, 1001, 224, -84, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 1, 224, 1, 224, 223, 223, 1101, 91, 45, 225, 1102, 63, 5, 225, 1101, 52, 58, 225, 1102, 59, 63, 225, 1101, 23, 14, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 1008, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 329, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 344, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 359, 1001, 223, 1, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 374, 1001, 223, 1, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 389, 1001, 223, 1, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 404, 1001, 223, 1, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 419, 1001, 223, 1, 223, 1007, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 434, 1001, 223, 1, 223, 107, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 449, 1001, 223, 1, 223, 1008, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 464, 1001, 223, 1, 223, 1007, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 479, 1001, 223, 1, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 494, 1001, 223, 1, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 509, 101, 1, 223, 223, 8, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 524, 101, 1, 223, 223, 107, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 539, 1001, 223, 1, 223, 8, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 554, 101, 1, 223, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 569, 101, 1, 223, 223, 108, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 584, 1001, 223, 1, 223, 7, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 599, 101, 1, 223, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 614, 1001, 223, 1, 223, 1107, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 629, 101, 1, 223, 223, 1107, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 644, 1001, 223, 1, 223, 1108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 659, 101, 1, 223, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226]
+const readline = require('readline')
+const input = require('./input.js')
 
-input[225] = 1
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-console.log(input[20])
+const output = []
 
-const getInput = () => {
-  return 1
-}
-
-const sendOutput = value => {
-  console.log('output', value)
-}
-
-const run = (memory, instructionPointer = 0) => {
-  // const opcode = memory[instructionPointer]
+const run = async (memory, instructionPointer = 0) => {
   const [E, D = '0', C = 0, B = 0, A = 0] = (memory[instructionPointer] + '').split('').reverse()
   const opcode = parseInt(D + E, 10)
   const param1 = parseInt(C, 10)
   const param2 = parseInt(B, 10)
   const param3 = parseInt(A, 10)
 
-  console.log({instructionPointer})
+  rl.write(`INSTRUCTION: ${instructionPointer}\n`)
+  rl.write(`OPCODE: ${opcode} P1:${param1} P2:${param2} P3:${param3}\n`)
 
   switch (opcode) {
     case 1:
@@ -33,11 +29,10 @@ const run = (memory, instructionPointer = 0) => {
         const a = param1 === 1 ? pointerA : memory[pointerA]
         const b = param2 === 1 ? pointerB : memory[pointerB]
 
-        if (param3 === 1) {
-          memory[instructionPointer + 3] = a + b
-        } else {
-          memory[pointerC] = a + b
-        }
+        // write
+        rl.write(`ADD: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${memory[pointerC]}:${a + b} \n`)
+
+        memory[pointerC] = a + b
 
         return run(memory, instructionPointer + 4)
       }
@@ -51,26 +46,62 @@ const run = (memory, instructionPointer = 0) => {
         const a = param1 === 1 ? pointerA : memory[pointerA]
         const b = param2 === 1 ? pointerB : memory[pointerB]
 
-        if (param3 === 1) {
-          memory[instructionPointer + 3] = a * b
-        } else {
-          memory[pointerC] = a * b
-        }
+        // write
+        rl.write(`MULTIPLY: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${a * b} \n`)
+
+        memory[pointerC] = a * b
 
         return run(memory, instructionPointer + 4)
       }
     case 3:
-      memory[instructionPointer + 1] = getInput()
-      return run(memory, instructionPointer + 2)
+      {
+        const q = new Promise(resolve => {
+          rl.question('>>> ', answer => {
+            if (!/[0-9]+/.test(answer)) {
+              throw new Error('Intcode computer only accepts numbers')
+            }
+
+            resolve(parseInt(answer, 10))
+          })
+        })
+
+        const value = await q
+        const pointerA = memory[instructionPointer + 1]
+
+        rl.write(`IN: ${value} -> ${pointerA}\n`)
+
+        // XXX is this needed?
+        if (param1 === 1) {
+          memory[instructionPointer + 1] = value
+        } else {
+          memory[pointerA] = value
+        }
+        return run(memory, instructionPointer + 2)
+      }
     case 4:
-      sendOutput(memory[instructionPointer + 1])
-      return run(memory, instructionPointer + 2)
+      {
+        const pointerA = memory[instructionPointer + 1]
+        // const value = memory[pointerA]
+        const value = param1 === 1
+          ? pointerA
+          : memory[pointerA]
+
+        output.push(value)
+        rl.write(`OUT: ${value}\n`)
+        return run(memory, instructionPointer + 2)
+      }
     case 99:
       // finished
-      return memory[0]
+      rl.write(`Finished: ${memory[0]}\n`)
+      rl.write(`Outputs: ${output.join(',')}\n`)
+      process.exit()
+    default:
+      rl.write(`Unknown instruction at position ${instructionPointer}: ${memory[instructionPointer]}\n`)
+      rl.close()
+      process.exit(1)
   }
 }
 
-const part1 = run(input)
+// input[6] = 1101
 
-console.log({part1})
+run(input)
