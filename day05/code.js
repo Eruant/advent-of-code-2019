e86690ba7adb0eed11979c5b55612d35833381fd
Matt Gale <matt@littleball.co.uk>
2019-12-07 22:37:24 +0000

    Day 5 - Part 2
    So much simpler than part 1!

--- day05/code.js
+++ day05/code.js
@@ -6,17 +6,12 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-const output = []
-
 const run = async (memory, instructionPointer = 0) => {
   const [E, D = '0', C = 0, B = 0, A = 0] = (memory[instructionPointer] + '').split('').reverse()
   const opcode = parseInt(D + E, 10)
   const param1 = parseInt(C, 10)
   const param2 = parseInt(B, 10)
   const param3 = parseInt(A, 10)
-
-  rl.write(`INSTRUCTION: ${instructionPointer}\n`)
-  rl.write(`OPCODE: ${opcode} P1:${param1} P2:${param2} P3:${param3}\n`)
 
   switch (opcode) {
     case 1:
@@ -30,7 +25,7 @@ const run = async (memory, instructionPointer = 0) => {
         const b = param2 === 1 ? pointerB : memory[pointerB]
 
         // write
-        rl.write(`ADD: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${memory[pointerC]}:${a + b} \n`)
+        rl.write(` ADD: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${memory[pointerC]}:${a + b} \n`)
 
         memory[pointerC] = a + b
 
@@ -47,7 +42,7 @@ const run = async (memory, instructionPointer = 0) => {
         const b = param2 === 1 ? pointerB : memory[pointerB]
 
         // write
-        rl.write(`MULTIPLY: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${a * b} \n`)
+        rl.write(` MULTIPLY: ${memory[instructionPointer + 1]}:${a} ${memory[instructionPointer + 2]}:${b} ${a * b} \n`)
 
         memory[pointerC] = a * b
 
@@ -68,7 +63,7 @@ const run = async (memory, instructionPointer = 0) => {
         const value = await q
         const pointerA = memory[instructionPointer + 1]
 
-        rl.write(`IN: ${value} -> ${pointerA}\n`)
+        rl.write(` IN: ${value} -> ${pointerA}\n`)
 
         // XXX is this needed?
         if (param1 === 1) {
@@ -86,14 +81,58 @@ const run = async (memory, instructionPointer = 0) => {
           ? pointerA
           : memory[pointerA]
 
-        output.push(value)
-        rl.write(`OUT: ${value}\n`)
+        rl.write(`--------------------\n OUT: ${value}\n--------------------\n`)
         return run(memory, instructionPointer + 2)
+      }
+    case 5:
+      {
+        const pointerA = memory[instructionPointer + 1]
+        const pointerB = memory[instructionPointer + 2]
+
+        const isTrue = (param1 === 1 ? pointerA : memory[pointerA]) !== 0
+        const next = param2 === 1 ? pointerB : memory[pointerB]
+
+        rl.write(` JUMP-IF-TRUE ${isTrue} ${next}\n`)
+        return run(memory, isTrue ? next : instructionPointer + 3)
+      }
+    case 6:
+      {
+        const pointerA = memory[instructionPointer + 1]
+        const pointerB = memory[instructionPointer + 2]
+
+        const isFalse = (param1 === 1 ? pointerA : memory[pointerA]) === 0
+        const next = param2 === 1 ? pointerB : memory[pointerB]
+
+        rl.write(` JUMP-IF-FALSE ${isFalse} ${next}\n`)
+        return run(memory, isFalse ? next : instructionPointer + 3)
+      }
+    case 7:
+      {
+        const pointerA = memory[instructionPointer + 1]
+        const pointerB = memory[instructionPointer + 2]
+        const pointerC = memory[instructionPointer + 3]
+
+        const a = param1 === 1 ? pointerA : memory[pointerA]
+        const b = param2 === 1 ? pointerB : memory[pointerB]
+
+        memory[pointerC] = (a < b) ? 1 : 0
+        return run(memory, instructionPointer + 4)
+      }
+    case 8:
+      {
+        const pointerA = memory[instructionPointer + 1]
+        const pointerB = memory[instructionPointer + 2]
+        const pointerC = memory[instructionPointer + 3]
+
+        const a = param1 === 1 ? pointerA : memory[pointerA]
+        const b = param2 === 1 ? pointerB : memory[pointerB]
+
+        memory[pointerC] = (a === b) ? 1 : 0
+        return run(memory, instructionPointer + 4)
       }
     case 99:
       // finished
       rl.write(`Finished: ${memory[0]}\n`)
-      rl.write(`Outputs: ${output.join(',')}\n`)
       process.exit()
     default:
       rl.write(`Unknown instruction at position ${instructionPointer}: ${memory[instructionPointer]}\n`)
@@ -101,7 +140,5 @@ const run = async (memory, instructionPointer = 0) => {
       process.exit(1)
   }
 }
-
-// input[6] = 1101
 
 run(input)
